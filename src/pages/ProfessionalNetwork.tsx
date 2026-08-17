d62eae6187a96509c9973f9efe36babcc274a4c6
davmorstein-ux <davmorstein@gmail.com>
2026-08-17 12:05:42 -0700

    Update ProfessionalNetwork.tsx

--- src/pages/ProfessionalNetwork.tsx
+++ src/pages/ProfessionalNetwork.tsx
@@ -7,7 +7,6 @@ import BreadcrumbSchema from "@/components/BreadcrumbSchema";
 import { articleSchema } from "@/lib/schema";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import iconPhone3d from "@/assets/icons/real-estate-phone-contact-icon-washington.webp";
 
 
 const professionalCategories = [
@@ -200,7 +199,6 @@ const ProfessionalNetwork = () => {
                   size="lg"
  className="hover:-light"
                 >
-                  <img src={iconPhone3d} alt="" aria-hidden="true" className="w-5 h-5 mr-2 object-contain shrink-0"  loading="lazy" sizes="100vw" decoding="async" width={1024} height={1536} />
                   Contact Real Property Planning
                 </Button>
               </Link>
